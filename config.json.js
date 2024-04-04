e466d20b83002f36c7f4e5e9e55f05572bd3e02c
MetaSquared <118878149+MetaSquared@users.noreply.github.com>
2024-04-04 09:14:29 +0800

    Update config.json.js

--- config.json.js
+++ config.json.js
@@ -2,10 +2,10 @@
     "author": "2TimesMeta", 
     "description": "DDNS-Go：自动获得你的公网 IPv4 或 IPv6 地址，并解析到对应的域名服务。", 
     "home_url": "Module_ddnsgo.asp", 
-    "link": "https://github.com/2TimesMeta/Koolcenter_ddns-go", 
+    "link": "https://github.com/MetaSquared/Koolcenter_ddns-go", 
     "md5": "", 
     "module": "ddnsgo", 
     "tags": "DDNS", 
     "title": "DDNS-Go", 
-    "version": "1.0.6"
+    "version": "1.0.7"
 }

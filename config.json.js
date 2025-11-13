e44aab3dc9d6bba76e0ea90b6029db82ba2a9535
sadog <sadoneli@gmail.com>
2025-11-13 09:49:29 +0800

    Update md5 field in config.json.js

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "description": "DDNS-Go：自动获得你的公网 IPv4 或 IPv6 地址，并解析到对应的域名服务。", 
     "home_url": "Module_ddnsgo.asp", 
     "link": "https://github.com/MetaSquared/Koolcenter_ddns-go", 
-    "md5": "", 
+    "md5": "xxx", 
     "module": "ddnsgo", 
     "tags": "DDNS", 
     "title": "DDNS-Go", 
